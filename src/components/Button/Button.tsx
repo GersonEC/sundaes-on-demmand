99b85c1468783ba8c024f7e9c92f89254798ebe5
GersonEC <gersonenriquez94@gmail.com>
2021-08-02 17:22:18 +0200

    Test and code button click.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import "./Button.css";
+
 export function Button() {
+  const [buttonColor, setButtonColor] = useState("red");
+  const newButtonColor = buttonColor === "red" ? "blue" : "red";
+
   return (
     <div>
-      <button>Button component</button>
+      <button
+        style={{ backgroundColor: buttonColor }}
+        onClick={() => setButtonColor(newButtonColor)}
+      >
+        Change to {newButtonColor}
+      </button>
     </div>
   );
 }
--- src/components/Button/button.test.js
+++ src/components/Button/button.test.js
@@ -1,8 +1,28 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Button } from "./Button";
 
 describe("Button", () => {
-  test("it has correct initial color", () => {
+  test("it has correct initial color set to red", () => {
     render(<Button />);
+    const button = screen.getByRole("button");
+    expect(button).toHaveStyle(`background-color: red`);
+  });
+
+  test("it has correct initial text 'change to blue'", () => {
+    const buttonText = /change to blue/i;
+    render(<Button />);
+    const button = screen.getByRole("button");
+    expect(button).toHaveTextContent(buttonText);
+  });
+
+  test("it turns blue when clicked", () => {
+    const buttonText = /change to blue/i;
+    const buttonTextAfterClick = /change to red/i;
+    render(<Button />);
+    const button = screen.getByRole("button", { name: buttonText });
+    userEvent.click(button);
+    expect(button).toHaveStyle(`background-color: blue`);
+    expect(button).toHaveTextContent(buttonTextAfterClick);
   });
 });
